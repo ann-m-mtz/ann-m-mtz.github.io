73c0120e2fed07513df9dfe053cb63fd7a545f29
Ana Muñoz <anacristina.mmtz@gmail.com>
2026-08-01 19:02:40 -0600

    Cambios en el UX/UI
    Swiper, información añadida al Home

--- js/main.js
+++ js/main.js
@@ -18,3 +18,148 @@ window.addEventListener("scroll", () => {
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
+
+//Slider Prueba
+
+//Slider Mapu - Primeros
+
+const primerosSwiper = new Swiper(".primerosSwiper", {
+    loop: true,
+    spaceBetween: 20,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1.2,
+    spaceBetween: 2,
+    breakpoints: {
+        768: {
+            slidesPerView: 2.3,
+        },
+        992: {
+            slidesPerView: 3.3,
+        }
+    }
+});
+
+const crecimientoSwiper = new Swiper(".crecimientoSwiper", {
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1.2,
+    spaceBetween: 2,
+
+    breakpoints: {
+        768: {
+            slidesPerView: 2.3,
+        },
+        992: {
+            slidesPerView: 3.3,
+        }
+    }
+});
+
+const expansionSwiper = new Swiper(".expansionSwiper", {
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1.2,
+    spaceBetween: 2,
+    breakpoints: {
+        768: {
+            slidesPerView: 2.3
+        },
+        992: {
+            slidesPerView: 3.3,
+        }
+    }
+
+});
+
+const otrosSwiper = new Swiper(".otrosSwiper", {
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1.2,
+    spaceBetween: 2,
+    breakpoints: {
+        768: {
+            slidesPerView: 2.3,
+        },
+        992: {
+            slidesPerView: 3.3,
+        }
+    }
+});
+
+//Revista
+
+const revistaSwiper = new Swiper(".revistaSwiper", {
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1.2,
+    spaceBetween: 8,
+    breakpoints: {
+        768: {
+            slidesPerView: 2.3,
+        },
+        992: {
+            slidesPerView: 3.3,
+        }
+    }
+});
+
+
+//Prevención
+
+const prevencionSwiper = new Swiper(".prevencionSwiper", {
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1.2,
+    spaceBetween: 2,
+    breakpoints: {
+        768: {
+            slidesPerView: 2.3,
+        },
+        992: {
+            slidesPerView: 3.3,
+        }
+    }
+});
+
+// Packaging
+
+const packagingSwiper = new Swiper(".packagingSwiper", {
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 1.2,
+    spaceBetween: 2,
+    breakpoints: {
+        768: {
+            slidesPerView: 2.3,
+        },
+        992: {
+            slidesPerView: 3.3,
+        }
+    }
+});
+
+//Educación
+const educacionSwiper = new Swiper(".educacionSwiper", {
+    loop:true,
+    grabCursor:true,
+    centeredSlides:true,
+    slidesPerView:1.1,
+    spaceBetween:15
+});
+
+//Ilustración
+
+const ilustracionSwiper = new Swiper(".ilustracionSwiper", {
+    loop:true,
+    grabCursor:true,
+    centeredSlides:true,
+    slidesPerView:1.1,
+    spaceBetween:15
+});
