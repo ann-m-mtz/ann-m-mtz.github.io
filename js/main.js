068305898e36aef925561c9d912178097ef7a543
Ana Muñoz <anacristina.mmtz@gmail.com>
2026-08-13 19:17:07 -0600

    Mejoras y optimización

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ window.addEventListener("scroll", () => {
 
     const header = document.querySelector("header");
 
-    console.log(window.scrollY);
-
     if (window.scrollY > 50) {
         header.classList.add("scrolled");
     } else {
